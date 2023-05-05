4fca225715512271a77da6dea6fa57885a919086
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-05-05 13:10:09 +0530

    updated traceExporter url

--- tracing.ts
+++ tracing.ts
@@ -11,9 +11,9 @@ diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 const sdk = new opentelemetry.NodeSDK({
   // traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   traceExporter: new OTLPTraceExporter({
-    url: "https://otel.dev2.zinclabs.dev/v1/traces",
+    url: "https://alpha1.gke.zinclabs.dev/api/default/traces",
     headers: {
-      Authorization: "Basic YWRtaW46Q29tcGxleHBhc3MjMTIz",
+      Authorization: "Basic cm9vdEBleGFtcGxlLmNvbTpDb21wbGV4cGFzcyMxMjM=",
     },
   }),
   instrumentations: [getNodeAutoInstrumentations()],
